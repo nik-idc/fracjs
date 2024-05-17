78b50108b85138641c2ec231be8c5283a6efc24f
atikin <nikvmain@gmail.com>
2024-05-17 14:49:03 +0300

    sierp parallel & koch very basic split

--- index.js
+++ index.js
@@ -1,22 +1,128 @@
-// const p = new Parallel([1, 2, 3]);
+import { SierpinskiTriangle, sierpTriangleSplit } from "./src/sierp.js";
+import { KochSnowflake, kochSplit } from "./src/koch.js";
 
-// console.log(p);
-import { Point } from "./src/shared/point.js";
-import { EqTriangle } from "./src/shared/triangle.js";
-import { SierpinskiTriangle } from "./src/sierp.js";
-
+const fractalSelect = document.getElementById("fractalSelect");
+const stOption = document.getElementById("stOption");
+const ksOption = document.getElementById("ksOption");
+const linearIterateButton = document.getElementById("linearIterateButton");
+const parallelIterateButton = document.getElementById("parallelIterateButton");
+const goBackButton = document.getElementById("goBackButton");
 const canvas = document.getElementById("canvas");
+
 const context = canvas.getContext("2d");
 
-const initialTriangle = new EqTriangle(
-  new Point(0, canvas.height),
-  canvas.height
-);
-const st = new SierpinskiTriangle(initialTriangle);
-st.draw(context, canvas.width, canvas.height);
+let fractalType = localStorage.getItem("lastType");
+let sierpinskiTriangle;
+let kochSnowflake;
 
-const iterateButton = document.getElementById("iterateButton");
-iterateButton.addEventListener("click", (ev) => {
-  st.iterate();
-  st.draw(context, canvas.width, canvas.height);
+function sierpinskiInit() {
+  const { Point, Triangle } = sierpTriangleSplit();
+  const initialTriangle = Triangle(Point(0, canvas.height), canvas.height);
+  sierpinskiTriangle = new SierpinskiTriangle(initialTriangle);
+  sierpinskiTriangle.draw(context, canvas.width, canvas.height);
+}
+
+function kochInit() {
+  const { Line, Point } = kochSplit();
+  const size = canvas.height;
+  const initLines = [Line(Point(0, size / 2), Point(size, size / 2), true)];
+
+  kochSnowflake = new KochSnowflake(initLines);
+  kochSnowflake.draw(context, canvas.width, canvas.height);
+}
+
+function init(type) {
+  console.log(type);
+  fractalType = type === null ? "sierspinskiTriangle" : type;
+  if (fractalType === "sierspinskiTriangle") {
+    stOption.setAttribute("selected", "selected");
+    ksOption.removeAttribute("selected");
+
+    sierpinskiInit();
+  } else if (fractalType === "kochSnowflake") {
+    ksOption.setAttribute("selected", "selected");
+    stOption.removeAttribute("selected");
+
+    kochInit();
+  }
+}
+
+fractalSelect.addEventListener("change", (ev) => {
+  localStorage.setItem("lastType", ev.target.value);
+  init(ev.target.value);
 });
+
+linearIterateButton.addEventListener("click", (ev) => {
+  if (fractalType === "sierspinskiTriangle") {
+    const result = sierpinskiTriangle.iterate();
+    if (!result) {
+      alert("Maximum depth reached");
+      return;
+    }
+
+    sierpinskiTriangle.draw(context, canvas.width, canvas.height);
+  } else if (fractalType === "kochSnowflake") {
+    const result = kochSnowflake.iterate();
+    if (!result) {
+      alert("Maximum depth reached");
+      return;
+    }
+
+    kochSnowflake.draw(context, canvas.width, canvas.height);
+  }
+});
+
+parallelIterateButton.addEventListener("click", (ev) => {
+  linearIterateButton.disabled = true;
+  parallelIterateButton.disabled = true;
+  goBackButton.disabled = true;
+
+  if (fractalType === "sierspinskiTriangle") {
+    sierpinskiTriangle
+      .parallelIterate(Parallel)
+      .then((result) => {
+        if (!result) {
+          alert("Maximum depth reached");
+          return;
+        }
+
+        sierpinskiTriangle.draw(context, canvas.width, canvas.height);
+
+        linearIterateButton.disabled = false;
+        parallelIterateButton.disabled = false;
+        goBackButton.disabled = false;
+      })
+      .catch((error) => {
+        console.log(error);
+
+        linearIterateButton.disabled = false;
+        parallelIterateButton.disabled = false;
+        goBackButton.disabled = false;
+      });
+  } else if (fractalType === "kochSnowflake") {
+    kochSnowflake
+      .parallelIterate(Parallel)
+      .then((result) => {
+        if (!result) {
+          alert("Maximum depth reached");
+          return;
+        }
+
+        kochSnowflake.draw(context, canvas.width, canvas.height);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+});
+
+goBackButton.addEventListener("click", (ev) => {
+  if (fractalType === "sierspinskiTriangle") {
+    sierpinskiTriangle.goBack();
+
+    sierpinskiTriangle.draw(context, canvas.width, canvas.height);
+  } else if (fractalType === "kochSnowflake") {
+  }
+});
+
+init(fractalType);
--- src/sierp.js
+++ src/sierp.js
@@ -1,47 +1,116 @@
-import { Point } from "./shared/point.js";
-import { EqTriangle } from "./shared/triangle.js";
+export function sierpTriangleSplit() {
+  const Point = (x, y) => {
+    return {
+      x: x,
+      y: y,
+    };
+  };
 
-export class SierpinskiTriangle {
-  /**
-   *
-   * @param {EqTriangle} initialTriangle
-   */
-  constructor(initialTriangle) {
-    this.prevLevels = [];
-    this.curLevel = [initialTriangle];
-    this.curLevelNum = 1;
-  }
+  const points = (p, size) => {
+    const p1 = Point(p.x, p.y);
+    const p2 = Point(p.x + size, p.y);
+    const p3 = Point(p.x + size / 2, p.y - size);
 
-  static sierpTriangleSplit(triangle) {
+    return [p1, p2, p3];
+  };
+
+  const Triangle = (p, size) => {
+    return { p: p, size: size, points: points(p, size) };
+  };
+
+  function split(triangle) {
     const newSize = triangle.size / 2;
 
-    const p1 = new Point(triangle.p.x, triangle.p.y);
-    const leftTriangle = new EqTriangle(p1, newSize);
+    const p1 = Point(triangle.p.x, triangle.p.y);
+    const leftTriangle = Triangle(p1, newSize);
 
-    const p2 = new Point(triangle.p.x + newSize, triangle.p.y);
-    const rightTriangle = new EqTriangle(p2, newSize);
+    const p2 = Point(triangle.p.x + newSize, triangle.p.y);
+    const rightTriangle = Triangle(p2, newSize);
 
-    const p3 = new Point(triangle.p.x + newSize / 2, triangle.p.y - newSize);
-    const topTriangle = new EqTriangle(p3, newSize);
+    const p3 = Point(triangle.p.x + newSize / 2, triangle.p.y - newSize);
+    const topTriangle = Triangle(p3, newSize);
 
     return [leftTriangle, rightTriangle, topTriangle];
   }
 
+  return { Point, points, Triangle, split };
+}
+
+export class SierpinskiTriangle {
+  constructor(initialTriangle) {
+    this.prevLevels = [];
+    this.curLevel = [initialTriangle];
+    this.curLevelNum = 1;
+    this.maxLevel = 12;
+  }
+
   iterate() {
+    if (this.curLevelNum >= this.maxLevel) {
+      return false;
+    }
+
     this.prevLevels.push(this.curLevel);
     this.curLevel = [];
     this.curLevelNum++;
     const prevLevel = this.prevLevels[this.prevLevels.length - 1];
 
+    const before = performance.now();
     for (const triangle of prevLevel) {
-      this.curLevel.push(...SierpinskiTriangle.sierpTriangleSplit(triangle));
+      this.curLevel.push(...sierpTriangleSplit().split(triangle));
     }
 
     console.log(this.curLevel);
+
+    console.log(
+      `Completed in ${performance.now() - before} ms\n` +
+        `Triangles count: ${this.curLevel.length}\n` +
+        `Current level: ${this.curLevelNum}\n\n`
+    );
+
+    return true;
   }
 
-  static degToRad(degrees) {
-    return (degrees * Math.PI) / 180;
+  parallelIterate() {
+    if (this.curLevelNum >= this.maxLevel) {
+      return false;
+    }
+
+    return new Promise((resolve, reject) => {
+      const curLevel = this.curLevel;
+
+      const p = new Parallel(curLevel, {
+        evalPath: "../eval.js",
+      });
+
+      p.require({
+        fn: sierpTriangleSplit,
+        name: "sierpTriangleSplit",
+      });
+
+      const before = performance.now();
+      p.map((triangle) => {
+        return sierpTriangleSplit().split(triangle);
+      }).then(
+        (data) => {
+          this.prevLevels.push(this.curLevel);
+          this.curLevel = [].concat(...data);
+          this.curLevelNum++;
+
+          console.log(this.curLevel);
+
+          console.log(
+            `Completed in ${performance.now() - before} ms\n` +
+              `Triangles count: ${this.curLevel.length}\n` +
+              `Current level: ${this.curLevelNum}\n\n`
+          );
+
+          resolve(true);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
   }
 
   /**
@@ -52,7 +121,7 @@ export class SierpinskiTriangle {
    */
   draw(context, canvasWidth, canvasHeight) {
     context.clearRect(0, 0, canvasWidth, canvasHeight);
-    
+
     for (const triangle of this.curLevel) {
       const points = triangle.points;
 
@@ -64,5 +133,21 @@ export class SierpinskiTriangle {
       context.lineTo(points[0].x, points[0].y);
       context.fill();
     }
+  }
+
+  goBack() {
+    if (this.curLevelNum === 1) {
+      return;
+    }
+
+    this.curLevel = this.prevLevels[this.prevLevels.length - 1];
+    this.curLevelNum--;
+    this.prevLevels.splice(this.prevLevels.length - 1);
+
+    console.log(
+      "Went back" +
+        `Triangles count: ${this.curLevel.length}\n` +
+        `Current level: ${this.curLevelNum}\n\n`
+    );
   }
 }
